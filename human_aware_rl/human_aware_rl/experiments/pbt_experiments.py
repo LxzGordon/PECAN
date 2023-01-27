d42e2effd15dcbbb88d2f526b560d536c4858ac2
LxzGordon <34240890+LxzGordon@users.noreply.github.com>
2023-01-27 17:05:51 +0000

    Update pbt_experiments.py

--- human_aware_rl/human_aware_rl/experiments/pbt_experiments.py
+++ human_aware_rl/human_aware_rl/experiments/pbt_experiments.py
@@ -1,5 +1,4 @@
 import time, gym, copy, seaborn
-#from human_aware_rl.pbt.pbt import PBT_DATA_DIR
 import numpy as np
 import tensorflow as tf
 import torch as th
@@ -13,7 +12,6 @@ from overcooked_ai_py.agents.benchmarking import AgentEvaluator
 from human_aware_rl.utils import set_global_seed, prepare_nested_default_dict_for_pickle, common_keys_equal
 from human_aware_rl.baselines_utils import get_pbt_agent_from_config_eval,get_pbt_agent_from_config_context
 from human_aware_rl.imitation.behavioural_cloning import get_bc_agent_from_saved
-#from human_aware_rl.pbt.pbt import PBT_DATA_DIR
 
 PBT_DATA_DIR='pbt_data_dir_2/'
 # Visualization
@@ -82,14 +80,8 @@ def evaluate_pbt_for_layout(layout_name, num_rounds, pbt_performance, pbt_model_
     assert common_keys_equal(bc_params["mdp_params"], pbt_config["mdp_params"]), "Mdp params differed between PBT and BC models training"
     assert common_keys_equal(bc_params["env_params"], pbt_config["env_params"]), "Env params differed between PBT and BC models training"
 
-    #for i in range(70,110,10):
-    for i in [999]:
-        print('ckp ',i)
-        pbt_agents = [get_pbt_agent_from_config_context(pbt_save_dir, pbt_config["sim_threads"], seed=seed, agent_idx=0, best=best,iter=i) for seed in seeds]
-        eval_pbt_over_seeds(pbt_agents, bc_agent, layout_name, num_rounds, pbt_performance, ae)
-    #return pbt_performance
-    #pbt_agents = [get_pbt_agent_from_config_context(pbt_save_dir, pbt_config["sim_threads"], seed=seed, agent_idx=0, best=best) for seed in seeds]
-    #eval_pbt_over_seeds(pbt_agents, bc_agent, layout_name, num_rounds, pbt_performance, ae)
+    pbt_agents = [get_pbt_agent_from_config_context(pbt_save_dir, pbt_config["sim_threads"], seed=seed, agent_idx=0, best=best) for seed in seeds]
+    eval_pbt_over_seeds(pbt_agents, bc_agent, layout_name, num_rounds, pbt_performance, ae)
     return pbt_performance
 
 def eval_pbt_over_seeds(pbt_agents, bc_agent, layout_name, num_rounds, pbt_performance, agent_evaluator):
@@ -97,19 +89,8 @@ def eval_pbt_over_seeds(pbt_agents, bc_agent, layout_name, num_rounds, pbt_perfo
     c_identifier=Net(layout_name).cuda()
     c_identifier.load_state_dict(th.load('context/net_level_'+layout_name+'.pth'))
 
-
     for i in range(len(pbt_agents)):
-        '''pbt_agents[i].context=np.array([0,1,0,0])
-        pbt_and_pbt = ae.evaluate_agent_pair_context(pbt_agents[i], pbt_agents[i], num_games=num_rounds)
-        avg_pbt_and_pbt = np.mean([a['ep_returns'] for a in pbt_and_pbt])
-        pbt_performance[layout_name]["PBT+PBT"].append(avg_pbt_and_pbt)'''
-
-        #pbt_agents[i].context=np.array([1,0,0,0])
-        #pbt_and_bc = ae.evaluate_agent_pair_context(pbt_agents[i], bc_agent, num_games=num_rounds,c_identifier=c_identifier)
-        #avg_pbt_and_bc = np.mean([a['ep_returns'] for a in pbt_and_bc])
-        #pbt_performance[layout_name]["PBT+BC_0"].append(avg_pbt_and_bc)
-        print('==========================================')
-        bc_and_pbt = ae.evaluate_agent_pair_context(bc_agent, pbt_agents[i], num_games=num_rounds,c_identifier=c_identifier)
+        bc_and_pbt = ae.evaluate_agent_pair_context(bc_agent, pbt_agents[i], num_games=num_rounds,c_identifier=c_identifier,embeddings=embeddings)
         avg_bc_and_pbt = np.mean([a['ep_returns'] for a in bc_and_pbt])
         pbt_performance[layout_name]["PBT+BC_1"].append(avg_bc_and_pbt)
         print(avg_bc_and_pbt)
@@ -118,9 +99,7 @@ def eval_pbt_over_seeds(pbt_agents, bc_agent, layout_name, num_rounds, pbt_perfo
 
 def run_all_pbt_experiments(best_bc_model_paths):
 
-    # best_bc_models = load_pickle("data/bc_runs/best_bc_models")
-    #seeds = [8015, 3554,  581, 5608, 4221] #581：33，1000：47
-    seeds=[8015]
+    seeds=[1111,5015,7015,8015]
     '''pbt_model_paths = {
         "simple": "pbt_simple",
         "unident_s": "pbt_unident_s",
@@ -129,7 +108,8 @@ def run_all_pbt_experiments(best_bc_model_paths):
         "random0": "pbt_random0"
     }'''
     pbt_model_paths = {
-      "random3": "pbt_random3",}
+      "simple": "pbt_simple"
+      }
 
     # Plotting
     plot_pbt_runs(pbt_model_paths, seeds, save=True)
@@ -144,10 +124,3 @@ best_bc=load_pickle("data/bc_runs/best_bc_model_paths")
 run_all_pbt_experiments(best_bc)
 performance=load_pickle(PBT_DATA_DIR + 'pbt_performance')
 print(performance)
-#performance=load_pickle(PBT_DATA_DIR + "pbt_performance_99995")
-#print(performance)
-# r3
-# 63.6 62.4 58.4 65.2
-# [74.57142857142857, 81.42857142857143, 85.71428571428571, 90.28571428571429]
-# 18 14 15.6 22.4
-#[30.285714285714285, 17.142857142857142, 25.142857142857142, 43.714285714285715]
